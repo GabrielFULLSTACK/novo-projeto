79b914452346e1791b1f2712c12a4b42a4987a52
gabriel marques <142952581+Gabrieel-Marques-Do-Nascimento@users.noreply.github.com>
2023-09-26 12:55:28 -0300

    atualizar

---
+++ desig/code.js
@@ -0,0 +1,24 @@
+
+//                                              funçã0 Ero
+const myObiserver = new IntersectionObserver((nome) => {
+    nome.forEach((nome) => {
+      // forEach pega elemento por element
+      if (nome.intersectionRatio) {
+        // isIntersecting : 'true ' = se tiver na vew port / 'False' = se tiver fora da vew port
+        nome.target.classList.add("show");
+   
+      } else {
+    
+        nome.target.classList.remove("show");
+      }
+    });
+  });
+  
+  const element = document.querySelectorAll(".hidden"); // querySelectorAll pega a class last-one
+  
+  // virou uma lista como varios elementos // pega elent por element // obiseva elemento por elemento
+  element.forEach((element) => myObiserver.observe(element));
+  
+  // vai observar lesOne   / inpecione  isIntersecting : 'true ' = se tiver na vew port / 'False' = se tiver fora da vew port
+  // myObiserver.observe(lastOne)
+  
