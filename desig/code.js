251844e200b69ad2cd3a993d19ef5a92d9e4422d
Gabriel marques do nascimento <142952581+Gabrieel-Marques-Do-Nascimento@users.noreply.github.com>
2023-10-13 05:28:13 -0300

    comm

--- desig/code.js
+++ desig/code.js
@@ -1,35 +1,27 @@
 // querySelectorAll pega todos elem com a class hidden
-  const element = document.querySelectorAll(".hidden");
-  
-  //                                              funçã0 Ero  
+const element = document.querySelectorAll('.hidden');
+
+//                                              funçã0 Ero
 const myObiserver = new IntersectionObserver((entr) => {
   entr.forEach((entr) => {
-      // forEach pega elemento por element
-      if (entr.intersectionRatio) {
-        // isIntersecting : 'true ' = se tiver na vew port / 'False' = se tiver fora da vew port
-        entr.target.classList.add("show");
-   
-      } else {
-    
-        entr.target.classList.remove("show");
-      }
-    });
+    // forEach pega elemento por element
+    if (entr.intersectionRatio) {
+      // isIntersecting : 'true ' = se tiver na vew port / 'False' = se tiver fora da vew port
+      
+      entr.target.classList.add("show");
+
+      
+    } else {
+      entr.target.classList.remove("show");
+      
+      
+    }
+    console.log(element)
   });
+});
 
+// virou uma lista como varios elementos // pega elent por element // obiseva elemento por elemento
+element.forEach((element) => myObiserver.observe(element));
 
-  
-  // virou uma lista como varios elementos // pega elent por element // obiseva elemento por elemento
-  element.forEach((element) => myObiserver.observe(element));
-  
-  // vai observar lesOne   / inpecione  isIntersecting : 'true ' = se tiver na vew port / 'False' = se tiver fora da vew port
-  // myObiserver.observe(lastOne)
-  
-
-
-
-
-
-
-
-
-
+// vai observar lesOne   / inpecione  isIntersecting : 'true ' = se tiver na vew port / 'False' = se tiver fora da vew port
+// myObiserver.observe(lastOne)
